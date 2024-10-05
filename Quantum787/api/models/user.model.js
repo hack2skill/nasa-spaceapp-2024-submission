301ca80627a28156b721dfe653447f2c18786385
Abhishek bhatt <104866090+abhishekbhattt@users.noreply.github.com>
2024-10-06 03:41:31 +0530

    Create user.model.js

---
+++ Quantum787/api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/011/209/565/original/user-profile-avatar-free-vector.jpg",
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
